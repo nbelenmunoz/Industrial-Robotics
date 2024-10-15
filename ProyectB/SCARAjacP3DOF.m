616dabf6ec2e3f95d1742cc9ef18a22a52564eb6
nbelenmunoz <76854563+nbelenmunoz@users.noreply.github.com>
2024-10-15 11:32:19 +0200

    Update SCARAjacP3DOF.m

--- ProyectB/SCARAjacP3DOF.m
+++ ProyectB/SCARAjacP3DOF.m
@@ -1,43 +1,40 @@
-function Jp = SCARAjacP3DOF(Q, Qp, L)
-
-% Extract joint variables and their derivatives
+function J = SCARAjac3DOF(Q, L)
+% Extract joint variables
 theta1 = Q(1);
 theta2 = Q(2);
 theta3 = Q(3);
-% d4 = Q(4); % Prismatic joint, not used in Jp calculation
 
-theta1_dot = Qp(1);
-theta2_dot = Qp(2);
-theta3_dot = Qp(3);
-% d4_dot = Qp(4); 
-
-% Extract link lengths
 l1 = L(1);
 l2 = L(2);
 l3 = L(3);
 
-% Compute cumulative angles and their derivatives
 theta12 = theta1 + theta2;
 theta123 = theta12 + theta3;
 
-theta12_dot = theta1_dot + theta2_dot;
-theta123_dot = theta12_dot + theta3_dot;
+J = zeros(4, 4);
 
-% Initialize the time derivative of the Jacobian matrix
-Jp = zeros(3, 4);
+% Partial derivatives of X 
+J(1, 1) = -l1 * sin(theta1) - l2 * sin(theta12) - l3 * sin(theta123);
+J(1, 2) = -l2 * sin(theta12) - l3 * sin(theta123);
+J(1, 3) = -l3 * sin(theta123);
+J(1, 4) = 0;
 
-% Compute time derivatives for X components
-Jp(1, 1) = -l1 * cos(theta1) * theta1_dot - l2 * cos(theta12) * theta12_dot - l3 * cos(theta123) * theta123_dot;
-Jp(1, 2) = -l2 * cos(theta12) * theta12_dot - l3 * cos(theta123) * theta123_dot;
-Jp(1, 3) = -l3 * cos(theta123) * theta123_dot;
-Jp(1, 4) = 0;
+% Partial derivatives of Y 
+J(2, 1) = l1 * cos(theta1) + l2 * cos(theta12) + l3 * cos(theta123);
+J(2, 2) = l2 * cos(theta12) + l3 * cos(theta123);
+J(2, 3) = l3 * cos(theta123);
+J(2, 4) = 0;
 
-% Compute time derivatives for Y components
-Jp(2, 1) = -l1 * sin(theta1) * theta1_dot - l2 * sin(theta12) * theta12_dot - l3 * sin(theta123) * theta123_dot;
-Jp(2, 2) = -l2 * sin(theta12) * theta12_dot - l3 * sin(theta123) * theta123_dot;
-Jp(2, 3) = -l3 * sin(theta123) * theta123_dot;
-Jp(2, 4) = 0;
+% Partial derivatives of Z 
+J(3, 1) = 0;
+J(3, 2) = 0;
+J(3, 3) = 0;
+J(3, 4) = 1; 
 
-% Compute time derivatives for Z components 
-Jp(3, :) = 0; 
+% Partial derivatives of phi 
+J(4, 1) = 1;
+J(4, 2) = 1;
+J(4, 3) = 1;
+J(4, 4) = 0;
+
 end
