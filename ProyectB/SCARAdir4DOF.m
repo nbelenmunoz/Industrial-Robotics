c5b9e7e27a88de6d584793d2fb3b820ab4a2e1cb
nbelenmunoz <76854563+nbelenmunoz@users.noreply.github.com>
2024-11-05 12:47:50 +0100

    Update and rename SCARAdir3DOF.m to SCARAdir4DOF.m

---
+++ ProyectB/SCARAdir4DOF.m
@@ -0,0 +1,18 @@
+function S = SCARAdir4DOF(Q, L)
+
+theta1 = Q(1);
+theta2 = Q(2);
+d3 = Q(3);    
+theta4 = Q(4);
+
+l1 = L(1);
+l2 = L(2);
+l3 = L(3);     
+
+S = zeros(3,1);
+
+S(1) = l1*cos(theta1) + l2*cos(theta1 + theta2) + l3*cos(theta1 + theta2 + theta4);
+S(2) = l1*sin(theta1) + l2*sin(theta1 + theta2) + l3*sin(theta1 + theta2 + theta4);
+S(3) = d3;
+
+end
