66a28fbe9a9fcc3983ffdaada0e8820276d67aaa
nbelenmunoz <76854563+nbelenmunoz@users.noreply.github.com>
2024-11-16 14:08:33 +0100

    Update SCARAjacdin4DOF.m

--- ProyectB/SCARAjacdin4DOF.m
+++ ProyectB/SCARAjacdin4DOF.m
@@ -1,22 +1,34 @@
 function J = SCARAjacdin4DOF(Q, L)
-    % SCARAjacdin4DOF computes the Jacobian matrix for a 3R SCARA robot
-    l1 = L(1); l2 = L(2); l3 = L(3);
-    g1 = L(4); g2 = L(5); g3 = L(6);
-    J = zeros(8, 3);
-    J(1,1) = -l1*sin(Q(1)) - l2*sin(Q(1) + Q(2)) - l3*sin(Q(1) + Q(2) + Q(3));
-    J(1,2) = -l2*sin(Q(1) + Q(2)) - l3*sin(Q(1) + Q(2) + Q(3));
-    J(1,3) = -l3*sin(Q(1) + Q(2) + Q(3));
-    J(2,1) = l1*cos(Q(1)) + l2*cos(Q(1) + Q(2)) + l3*cos(Q(1) + Q(2) + Q(3));
-    J(2,2) = l2*cos(Q(1) + Q(2)) + l3*cos(Q(1) + Q(2) + Q(3));
-    J(2,3) = l3*cos(Q(1) + Q(2) + Q(3));
-    J(3,1) = -l1*sin(Q(1)) - g2*sin(Q(1) + Q(2)) - g3*sin(Q(1) + Q(2) + Q(3));
-    J(3,2) = -g2*sin(Q(1) + Q(2)) - g3*sin(Q(1) + Q(2) + Q(3));
-    J(3,3) = -g3*sin(Q(1) + Q(2) + Q(3));
-    J(4,1) = l1*cos(Q(1)) + g2*cos(Q(1) + Q(2)) + g3*cos(Q(1) + Q(2) + Q(3));
-    J(4,2) = g2*cos(Q(1) + Q(2)) + g3*cos(Q(1) + Q(2) + Q(3));
-    J(4,3) = g3*cos(Q(1) + Q(2) + Q(3));
-    J(5,1) = 1; J(5,2) = 1; J(5,3) = 1;
-    J(6,1) = -g1*sin(Q(1)); J(6,2) = 0; J(6,3) = 0;
-    J(7,1) = g1*cos(Q(1)); J(7,2) = 0; J(7,3) = 0;
-    J(8,1) = 1; J(8,2) = 0; J(8,3) = 0;
+
+theta1 = Q(1);
+theta2 = Q(2);
+d3     = Q(3); 
+theta3 = Q(4);
+
+l1 = L(1);
+l2 = L(2);
+l3 = L(3);
+
+theta12 = theta1 + theta2;
+theta123 = theta1 + theta2 + theta3;
+
+J = zeros(4, 4);
+
+J(1,1) = -l1 * sin(theta1) - l2 * sin(theta12) - l3 * sin(theta123);
+J(1,2) = -l2 * sin(theta12) - l3 * sin(theta123);
+J(1,3) = 0;
+J(1,4) = -l3 * sin(theta123);
+J(2,1) = l1 * cos(theta1) + l2 * cos(theta12) + l3 * cos(theta123);
+J(2,2) = l2 * cos(theta12) + l3 * cos(theta123);
+J(2,3) = 0;
+J(2,4) = l3 * cos(theta123);
+J(3,1) = 0;
+J(3,2) = 0;
+J(3,3) = 1;  
+J(3,4) = 0;
+J(4,1) = 1;
+J(4,2) = 1;
+J(4,3) = 0;
+J(4,4) = 1;
+
 end
