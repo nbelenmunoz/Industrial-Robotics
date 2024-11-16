fedf0f8eae731a57a3387d2a55eb7b057e43896f
nbelenmunoz <76854563+nbelenmunoz@users.noreply.github.com>
2024-11-16 14:09:59 +0100

    Update Test_Scara_dynamics_projects.m

--- ProyectB/Test_Scara_dynamics_projects.m
+++ ProyectB/Test_Scara_dynamics_projects.m
@@ -3,32 +3,35 @@ clear all; close all; clc;
 l1 = 0.33;  
 l2 = 0.33;  
 l3 = 0.33; 
-g1 = 0.28; 
-g2 = 0.283; 
-g3 = 0.283; 
 
+L = [l1; l2; l3];
 
-L = [l1; l2; l3; g1; g2; g3];
+m_x = 0.5;       
+m_y = 0.5;      
+m_z = 7.0;     
+I_theta = 0.0565; 
 
-M = diag([0.5, 0.5, 7, 7, 0.0565, 7.8, 7.8, 0.0565]); 
+M = diag([m_x, m_y, m_z, I_theta]); 
 
-Fse = zeros(8, 1);  
-Fse(1) = 1;
-Fse(2) = 1;
+Fse = zeros(4, 1);  
+Fse(1) = 1;  
+Fse(2) = 1;  
 
-Q = [pi/4, pi/3, pi/6, pi/8]; 
-Qp = [-2.66; -2.2; 1.5]; 
-Qpp = [26.63; -12.36; 5.0]; 
+Q = [pi/4; pi/3; pi/6; pi/8]; 
+Qp = [-2.66; -2.2; 1.5; 0.5];     
+Qpp = [26.63; -12.36; 5.0; 1.0];  
 
-Sd = SCARAdirdin4DOF(Q, L);
-J = SCARAjacdin4DOF(Q, L);
-Jp = SCARAjacPdin4DOF(Q, Qp, L);
+Sd = SCARAdirdin4DOF(Q, L);      
+J = SCARAjacdin4DOF(Q, L);       
+Jp = SCARAjacPdin4DOF(Q, Qp, L); 
 
 Spp = Jp * Qp + J * Qpp; 
-Fsi = -diag(M) .* Spp;  
 
-Fs = (Fse + Fsi);
+Fsi = -M * Spp;  
+
+Fs = Fse + Fsi;
+
 Fcq = -J' * Fs;
 
-% forces/torques
-display(Fcq); 
+disp('Forces/Torques at the joints:');
+disp(Fcq); 
