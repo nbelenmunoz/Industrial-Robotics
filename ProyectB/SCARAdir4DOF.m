9469f6b6ea89b422eaf63c2c3d5ee4d66b6ffcfc
nbelenmunoz <76854563+nbelenmunoz@users.noreply.github.com>
2024-11-24 22:00:08 +0100

    Update SCARAdir4DOF.m

--- ProyectB/SCARAdir4DOF.m
+++ ProyectB/SCARAdir4DOF.m
@@ -1,9 +1,10 @@
 function S = SCARAdir4DOF(Q, L)
 
 theta1 = Q(1);
-theta2 = Q(2);
-d3 = Q(3);    
-theta4 = Q(4);
+theta2 = Q(2);    
+theta3 = Q(3);
+d4 = Q(4);
+
 
 l1 = L(1);
 l2 = L(2);
@@ -11,8 +12,8 @@ l3 = L(3);
 
 S = zeros(3,1);
 
-S(1) = l1*cos(theta1) + l2*cos(theta1 + theta2) + l3*cos(theta1 + theta2 + theta4);
-S(2) = l1*sin(theta1) + l2*sin(theta1 + theta2) + l3*sin(theta1 + theta2 + theta4);
-S(3) = d3;
+S(1) = l1*cos(theta1) + l2*cos(theta1 + theta2) + l3*cos(theta1 + theta2 + theta3);
+S(2) = l1*sin(theta1) + l2*sin(theta1 + theta2) + l3*sin(theta1 + theta2 + theta3);
+S(3) = d4;
 
 end
