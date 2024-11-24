588cf0b348c7b1cbb369ab152d839c7af1849f3b
nbelenmunoz <76854563+nbelenmunoz@users.noreply.github.com>
2024-11-24 22:08:56 +0100

    Update PlotScara4DOF.m

--- ProyectB/PlotScara4DOF.m
+++ ProyectB/PlotScara4DOF.m
@@ -5,8 +5,8 @@ hold on;
 
 theta1 = Q(1);
 theta2 = Q(2);
-d3     = Q(3); 
-theta4 = Q(4);
+theta3 = Q(3);
+d4  = Q(4); 
 
 l1 = L(1);
 l2 = L(2);
@@ -21,8 +21,8 @@ y1 = l1 * sin(theta1);
 x2 = x1 + l2 * cos(theta1 + theta2);
 y2 = y1 + l2 * sin(theta1 + theta2);
 
-x3 = x2 + l3 * cos(theta1 + theta2 + theta4);
-y3 = y2 + l3 * sin(theta1 + theta2 + theta4);
+x3 = x2 + l3 * cos(theta1 + theta2 + theta3);
+y3 = y2 + l3 * sin(theta1 + theta2 + theta3);
 
 % Plot the robot links
 plot([x0, x1, x2, x3], [y0, y1, y2, y3], 'LineWidth', 2, 'Color', colore);
