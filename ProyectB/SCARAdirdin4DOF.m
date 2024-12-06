daa82005b5bee45fdd63b073f7178ffc4c103573
nbelenmunoz <76854563+nbelenmunoz@users.noreply.github.com>
2024-12-06 14:23:11 +0100

    Update SCARAdirdin4DOF.m

--- ProyectB/SCARAdirdin4DOF.m
+++ ProyectB/SCARAdirdin4DOF.m
@@ -1,11 +1,4 @@
-function S = SCARAdirdin4DOF(Q, L)
-    if length(Q) < 4
-        error('Q must have at least 4 elements: [Q1, Q2, Q3, Q4]');
-    end
-    if length(L) < 6
-        error('L must have at least 6 elements: [l1, l2, l3, g1, g2, g3]');
-    end
-
+function S = SCARAdirdin4DOF(Q, L, dd4, external_force_z, masses, payload)
     theta1 = Q(1);
     theta2 = Q(2);
     theta3 = Q(3);
@@ -18,9 +11,16 @@ function S = SCARAdirdin4DOF(Q, L)
     g2 = L(5);
     g3 = L(6);
 
-    % Initialize output vector
-    S = zeros(15, 1);
+    m1 = masses(1); 
+    m2 = masses(2); 
+    m3 = masses(3); 
 
+    m4 = m1 + m2 + m3 + payload; 
+
+    g = 9.81;
+
+    
+    S = zeros(16, 1); 
     % Cumulative angles
     theta12 = theta1 + theta2;
     theta123 = theta12 + theta3;
@@ -35,10 +35,10 @@ function S = SCARAdirdin4DOF(Q, L)
     S(5) = l1 * sin(theta1) + l2 * sin(theta12) + g3 * sin(theta123);
     S(6) = d4;
 
-    %Total orientation at the end-effector (phi)
+    % Total orientation at the end-effector (phi)
     S(7) = theta123;
 
-    %Position of the center of mass of link 2
+    % Position of the center of mass of link 2
     S(8) = l1 * cos(theta1) + g2 * cos(theta12);
     S(9) = l1 * sin(theta1) + g2 * sin(theta12);
     S(10) = d4;
@@ -51,6 +51,12 @@ function S = SCARAdirdin4DOF(Q, L)
     S(13) = g1 * sin(theta1);
     S(14) = d4;
 
-    %Orientation at joint 1
+    % Orientation at joint 1
     S(15) = theta1;
+
+    % Prismatic joint dynamics (Force in z-direction)
+    inertiaz = m4 * dd4;
+    gravityz = m4 * g;  
+    totalz = inertiaz + gravityz + external_force_z;
+    S(16) = totalz;
 end
