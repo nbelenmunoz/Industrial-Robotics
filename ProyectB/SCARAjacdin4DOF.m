73af2871ed1541c954cd1c9365112450c773ad22
nbelenmunoz <76854563+nbelenmunoz@users.noreply.github.com>
2024-12-06 14:23:47 +0100

    Update SCARAjacdin4DOF.m

--- ProyectB/SCARAjacdin4DOF.m
+++ ProyectB/SCARAjacdin4DOF.m
@@ -1,9 +1,11 @@
 function J = SCARAjacdin4DOF(Q, L)
+    % Extract joint variables
     theta1 = Q(1);
     theta2 = Q(2);
     theta3 = Q(3);
     d4     = Q(4);
     
+    % Extract link lengths and center of mass distances
     l1 = L(1);
     l2 = L(2);
     l3 = L(3);
@@ -14,10 +16,11 @@ function J = SCARAjacdin4DOF(Q, L)
     % Cumulative angles
     theta12 = theta1 + theta2;
     theta123 = theta12 + theta3;
-
-    J = zeros(15, 4);
     
-    %End-effector position
+    % Initialize Jacobian matrix
+    J = zeros(16, 4);
+    
+    % Rows 1-3: End-effector position
     J(1,1) = -l1*sin(theta1) - l2*sin(theta12) - l3*sin(theta123);
     J(1,2) = -l2*sin(theta12) - l3*sin(theta123);
     J(1,3) = -l3*sin(theta123);
@@ -33,7 +36,7 @@ function J = SCARAjacdin4DOF(Q, L)
     J(3,3) = 0;
     J(3,4) = 1;
     
-    %Center of mass of link 3
+    % Rows 4-6: Center of mass of link 3
     J(4,1) = -l1*sin(theta1) - l2*sin(theta12) - g3*sin(theta123);
     J(4,2) = -l2*sin(theta12) - g3*sin(theta123);
     J(4,3) = -g3*sin(theta123);
@@ -49,13 +52,13 @@ function J = SCARAjacdin4DOF(Q, L)
     J(6,3) = 0;
     J(6,4) = 1;
     
-    %Total orientation at end-effector
+    % Row 7: Total orientation at end-effector
     J(7,1) = 1;
     J(7,2) = 1;
     J(7,3) = 1;
     J(7,4) = 0;
     
-    %Center of mass of link 2
+    % Rows 8-10: Center of mass of link 2
     J(8,1) = -l1*sin(theta1) - g2*sin(theta12);
     J(8,2) = -g2*sin(theta12);
     J(8,3) = 0;
@@ -71,13 +74,13 @@ function J = SCARAjacdin4DOF(Q, L)
     J(10,3) = 0;
     J(10,4) = 1;
     
-    %Orientation at joint 2
+    % Row 11: Orientation at joint 2
     J(11,1) = 1;
     J(11,2) = 1;
     J(11,3) = 0;
     J(11,4) = 0;
     
-    % Center of mass of link 1
+    % Rows 12-14: Center of mass of link 1
     J(12,1) = -g1*sin(theta1);
     J(12,2) = 0;
     J(12,3) = 0;
@@ -93,10 +96,15 @@ function J = SCARAjacdin4DOF(Q, L)
     J(14,3) = 0;
     J(14,4) = 1;
     
-    % Orientation at joint 1
+    % Row 15: Orientation at joint 1
     J(15,1) = 1;
     J(15,2) = 0;
     J(15,3) = 0;
     J(15,4) = 0;
-end
     
+    % Row 16: Prismatic joint force dynamics (z-direction)
+    J(16,1) = 0;
+    J(16,2) = 0;
+    J(16,3) = 0;
+    J(16,4) = 1; 
+end
